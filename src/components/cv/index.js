9341a1683df3f0c744a0d55fe832ac199548133b
yasmin <yasmartins999@gmail.com>
2022-10-11 17:20:54 -0300

    buttons

--- src/components/cv/index.js
+++ src/components/cv/index.js
@@ -7,8 +7,7 @@ const Cv = () => {
     return (
         <div className={CvStyles.container} id="cv">
             <h1> Mi Curricullum </h1>
-            <button className={CvStyles.button}><a href="https://www.linkedin.com/feed/update/urn:li:activity:6920453701321011200/" download="cv ingles.pdf">Ingles</a></button>
-            <button className={CvStyles.button}><a href="https://www.linkedin.com/feed/update/urn:li:activity:6920453779221794816/" download="cv español.pdf">Español</a></button>
+            <button className={CvStyles.button}><a href="https://www.linkedin.com/feed/update/urn:li:activity:6920453701321011200/" download="cv ingles.pdf"> Cv </a></button>
         </div>
     );
 };
